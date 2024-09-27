178ac9263890ad2ebb0c32c33ff29ad2f4a5b614
AJ Caldwell <tomatrow@gmail.com>
2024-09-26 22:52:12 -0700

    Simplify counter

--- src/routes/Counter.svelte
+++ src/routes/Counter.svelte
@@ -1,28 +1,11 @@
-<script lang="ts">
-	import { marked } from 'marked';
-
-	let count: number = $state(0);
+<script>
+	let count = $state(0);
 
 	function increment() {
 		count += 1;
 	}
-
-	const html: string = $derived(marked.parse(`**Count**: _${count}_`));
 </script>
 
-<svelte:head>
-	<link rel="stylesheet" href="https://unpkg.com/normalize.css" />
-	<link rel="stylesheet" href="https://unpkg.com/magick.css" />
-</svelte:head>
-
-<main>
-	<button onclick={increment}>
-		{@html html}
-	</button>
-</main>
-
-<style>
-	button {
-		margin: 2rem;
-	}
-</style>
+<button onclick={increment}>
+	clicks: {count}
+</button>

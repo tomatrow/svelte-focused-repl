1d391f984aa48ec5b0c08689245bc2f794b6335b
AJ Caldwell <tomatrow@gmail.com>
2024-09-29 06:26:05 -0700

    Copy pwa layout setup from the example

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+	import { pwaInfo } from 'virtual:pwa-info';
+	import { pwaAssetsHead } from 'virtual:pwa-assets/head';
+
+	const { children } = $props();
+
+	const webManifest = pwaInfo?.webManifest?.linkTag ?? '';
+</script>
+
+<svelte:head>
+	<!-- begin -->
+	{#if pwaAssetsHead.themeColor}
+		<meta name="theme-color" content={pwaAssetsHead.themeColor.content} />
+	{/if}
+	{#each pwaAssetsHead.links as link}
+		<link {...link} />
+	{/each}
+	{@html webManifest}
+	<!-- end -->
+</svelte:head>
+
+{@render children?.()}
